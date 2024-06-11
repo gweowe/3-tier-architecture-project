c72369edb97a23a535bd8a65673b509d613a5c40
gweowe <gweowe@gmail.com>
2024-06-12 00:01:45 +0900

    add k8s install script

---
+++ k8s_install_script/helm.sh
@@ -0,0 +1,4 @@
+# helm 설치
+curl -o ~/helm_install.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 755 helm_install.sh
+./helm_install.sh
---
+++ k8s_install_script/worker_node.sh
@@ -0,0 +1,37 @@
+# 사전 OS 구성
+sudo systemctl disable --now firewalld
+sudo swapoff -a
+sudo modprobe br_netfilter
+sudo modprobe overlay
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sudo sysctl --system
+
+# k8s 설치
+sudo yum update -y
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key
+EOF
+sudo yum install kubelet kubeadm kubectl -y
+sudo systemctl enable --now kubelet
+
+# CRI(containerd) 설치
+sudo yum install yum-utils -y
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install containerd.io -y
+sudo rm -rf /etc/containerd/config.toml
+sudo systemctl enable --now containerd
+
+# master node init 후 join 작업 수행 필요
